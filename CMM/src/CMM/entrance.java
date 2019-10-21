e62fbdc28b80fad335bd619e2aa616aa931be93d
tgqgit <45258466+tgqgit@users.noreply.github.com>
2019-10-21 17:18:31 +0800

    Add files via upload

--- CMM/src/CMM/entrance.java
+++ CMM/src/CMM/entrance.java
@@ -7,8 +7,11 @@ import java.util.ArrayList;
 import CMM.parser;
 
 public class entrance {
+    //使用的基础数据结构
     public static ArrayList<token> tokenStream = new ArrayList<token>();
     public static ArrayList<token> tokenError = new ArrayList<>();  //错误的词素
+    //前后端交互的接口
+    public static String tokenResult = "";
     public static void main(String[] args){
 
         //1.词法分析开始
@@ -16,7 +19,7 @@ public class entrance {
         lexer le = new lexer();
 
         //源文件的txt的处理
-        String pathname = "src/cmmtest.txt";
+        String pathname = "src/cmmtest(2).txt";
         File file = new File(pathname);
         //txt 转换为 字符数组
         String source = "";
@@ -29,10 +32,8 @@ public class entrance {
         char sourcefile[] = source.toCharArray();
         //预处理 去掉注释等
         char afterfile[] = le.preLexer(sourcefile);
+        //输出预处理后的程序文件
         System.out.println(afterfile);
-//        System.out.println("!!!");
-//        System.out.println(afterfile[4]);
-//        System.out.println("!!!");
         //开始词法分析
         tokenStream = le.analyze(afterfile);
         int tempError = 0;
@@ -40,19 +41,26 @@ public class entrance {
         {
             int w = tokenStream.get(i).kind;
             if(w<0)
-                System.out.print("Error("+w+")"+"\t");
+                //System.out.print("Error("+w+")"+"\t");
+                tokenResult = tokenResult + "Error("+w+")"+"\t";
             else
-                System.out.print(w+"\t\t\t");
-            System.out.print(tokenStream.get(i).content+"\t\t\t");
-            System.out.print(tokenStream.get(i).line);
+                //System.out.print(w+"\t\t\t");
+                tokenResult = tokenResult + w+"\t\t\t";
+            //System.out.print(tokenStream.get(i).content+"\t\t\t");
+            tokenResult = tokenResult + tokenStream.get(i).content+"\t\t\t";
+            //System.out.print(tokenStream.get(i).line);
+            tokenResult = tokenResult + tokenStream.get(i).line;
             if(w<0)
             {
-                System.out.println(tokenError.get(tempError).content);
+                //System.out.println(tokenError.get(tempError).content);
+                tokenResult = tokenResult + tokenError.get(tempError).content + "\n";
                 ++tempError;
             }
             else
-                System.out.println();
+                //System.out.println();
+                tokenResult = tokenResult + "\n";
         }
+        System.out.println(tokenResult);
         //语法分析开始
         System.out.println("\n语法分析开始：\n");
         parser par = new parser();
